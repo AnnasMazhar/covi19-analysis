3661c6392bf93e47d11e1ae5e29bb9bcf22d6b13
Phantom <annas.mazhar10@gmail.com>
2020-04-19 23:27:29 +0500

    added statewise function and some changes in progress

--- covid_us.py
+++ covid_us.py
@@ -1,8 +1,28 @@
 import pandas as pd
 
+pd.set_option('display.max_columns', 10)
+pd.set_option('display.width', 120)
+
 
 def get_data(fileName):
 	data = pd.read_csv(fileName).drop(['UID','iso2','iso3','code3','FIPS','Admin2','Lat','Long_'], axis=1)
+	data['Province_State'].fillna('<all>', inplace=True)
 	return data
 
-print(get_data('time_series_covid19_confirmed_US.csv'))
+confirmed_us = get_data('time_series_covid19_confirmed_US.csv')
+deaths_us = get_data('time_series_covid19_deaths_US.csv')
+
+
+def by_state(dataset, state):
+	states = dataset['Province_State'].unique()
+	states.sort()
+	dates = list(dataset.columns[3:].unique())
+	statewise_report = pd.DataFrame(columns=dates, index = states)
+	# import pdb; pdb.set_trace()
+	print(dataset['Province_State'].value_counts())
+	for st in states:.sort
+		if st in dataset['Province_State']:
+			print(dataset.Province_State.mode())
+	return statewise_report
+
+by_state(confirmed_us, "Alabama")

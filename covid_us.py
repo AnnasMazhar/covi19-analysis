f4cdd3e08d6a6e5997de6f39c60eba05d0400d16
Anas <annas.mazhar10@gmail.com>
2020-04-20 14:30:07 +0500

    added selection by county, merged by_state function and fetch county from data, added baseUrl for live data from git ub api

--- covid_us.py
+++ covid_us.py
@@ -1,11 +1,12 @@
 import pandas as pd
 
 pd.set_option('display.max_columns', 10)
-pd.set_option('display.width', 120)
+pd.set_option('display.width', 140)
 
+base_url = "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/"
 
 def get_data(fileName):
-	data = pd.read_csv(fileName).drop(['UID','iso2','iso3','code3','FIPS','Admin2','Lat','Long_'], axis=1)
+	data = pd.read_csv(base_url+fileName).drop(['UID','iso2','iso3','code3','FIPS','Country_Region','Admin2','Lat','Long_'], axis=1)
 	data['Province_State'].fillna('<all>', inplace=True)
 	return data
 
@@ -17,13 +18,19 @@ def by_state(dataset, state):
 	states = dataset['Province_State'].unique()
 	states.sort()
 	dates = list(dataset.columns[3:].unique())
-	statewise_report = pd.DataFrame(columns=dates, index = states)
+	# statewise_report = pd.DataFrame(columns=dates, index = states)
 	# import pdb; pdb.set_trace()
-	for st in states:
-		state = dataset[dataset['Province_State'].str.contains(st)]
-		print(type(state))
-		# for dt in dates:
+	st = dataset[dataset['Province_State'] == state]#.str.contains(st)]
+	return st
 
-	return statewise_report
+# print(by_state(confirmed_us, "New York"))
 
-by_state(confirmed_us, "Alabama")
+def get_county(column):
+	return column.split(',')[0]
+
+def by_county(dataset, county):
+	# county = list(dataset['Combined_Key'].apply(lambda x:get_county(x)))
+	cty = dataset[dataset['Combined_Key'].str.match(county)]
+	print(cty)
+
+by_county(confirmed_us,"Wyoming")
